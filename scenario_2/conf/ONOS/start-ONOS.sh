f68eb67effaace8c79b3be9d236102d9a71f9402
sonsoleslp <sonsoleslp@hotmail.com>
2018-01-23 23:14:36 +0100

    Scenario 2

--- scenario_2/conf/ONOS/start-ONOS.sh
+++ scenario_2/conf/ONOS/start-ONOS.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-
-# Load bashrc
-PS1='$ ' . ~/.bashrc
-
-cd $KARAF_ROOT; ./bin/start
+export ONOS_APPS="drivers,openflow,proxyarp,sdnip,reactive-routing"
+service onos start
+sleep 30
+sudo systemctl start onos.service
+sleep 30
+/opt/onos/karaf/bin/client "app activate org.onosproject.drivers"
+sleep 10
+/opt/onos/karaf/bin/client "app activate org.onosproject.openflow"
+sleep 10
+/opt/onos/karaf/bin/client "app activate org.onosproject.proxyarp"
+sleep 10
+/opt/onos/karaf/bin/client "app activate org.onosproject.sdnip"
+sleep 10
+/opt/onos/karaf/bin/client "app activate org.onosproject.reactive-routing"
